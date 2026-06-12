54c989facfa1f8a14d3f3a3f0fa04227190121ed
Lisa Scheers <lisa@scheers.tech>
2026-06-12 23:26:52 +0200

    fix: use homeDirectory variable in IdentityAgent path for 1Password's SSH agent

--- home/lisa/modules/ssh.nix
+++ home/lisa/modules/ssh.nix
@@ -1,4 +1,6 @@
-{
+{config, ...}: let
+  homeDirectory = config.home.homeDirectory;
+in {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
@@ -91,7 +93,7 @@
       };
 
       "*" = {
-        IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+        IdentityAgent = "\"${homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
       };
     };
   };
